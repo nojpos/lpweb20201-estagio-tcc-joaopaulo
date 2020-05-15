19f4dee2d12aaeb5e70531392cecad8d2f027169
Jackson Gomes <jgomes@ceulp.edu.br>
2020-05-15 10:49:13 -0300

    refactor(frontend): atualiza componente app
    Atualiza o componente app para usar router-outlet

--- lpweb20201-estagio-tcc-front/src/app/app.component.ts
+++ lpweb20201-estagio-tcc-front/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
-import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,60 +6,9 @@ import { UserService } from './user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  user: any;
-  error: any;
-  usuarios: any;
-  usuarios_error: any;
+  constructor() { }
 
-  username = null;
-  password = null;
+  ngOnInit() { }
 
-  constructor(public auth$: AuthService, private user$: UserService) { }
-
-  ngOnInit() {
-    this.user = this.auth$.user();
-  }
-
-  login() {
-    this.auth$.login(this.username, this.password)
-      .subscribe(
-        user => this.user = user,
-        err => this.error = err.error
-      );
-  }
-
-  refreshToken() {
-    this.auth$.refreshToken().subscribe(
-      user => {
-        this.user = user;
-        this.error = null;
-      },
-      error => {
-        this.error = error.error;
-        this.user = null;
-      }
-    );
-  }
-
-  logout() {
-    this.auth$.logout();
-    this.user = null;
-    this.username = null;
-    this.password = null;
-  }
-
-  carregarUsuarios() {
-    this.user$.list()
-      .subscribe(
-        (dados: any) => {
-          this.usuarios = dados;
-          this.usuarios_error = null;
-        },
-        error => {
-          this.usuarios = null;
-          this.usuarios_error = error.error;
-        }
-      );
-  }
 }
 
