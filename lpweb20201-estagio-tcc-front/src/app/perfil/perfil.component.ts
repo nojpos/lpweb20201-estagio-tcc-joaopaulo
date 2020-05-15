35f9ced0f6b795cbd688d39500656eca6cdc8629
Jackson Gomes <jgomes@ceulp.edu.br>
2020-05-15 10:47:02 -0300

    feat(frontend): adiciona componente perfil

---
+++ lpweb20201-estagio-tcc-front/src/app/perfil/perfil.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { PerfilService } from '../perfil.service';
+import { AuthService } from '../auth.service';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent implements OnInit {
+  user: any;
+  perfil: any;
+  temPerfil = null;
+
+  constructor(public auth$: AuthService, private perfil$: PerfilService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.user = this.auth$.user();
+    if (this.user) {
+      this.perfil$.perfilLogado()
+        .subscribe(
+          dados => this.perfil = dados,
+          erro => this.temPerfil = false
+        );
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  logout() {
+    this.auth$.logout();
+    this.router.navigate(['/']);
+  }
+}
