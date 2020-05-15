5a5f8a6cf0dcfd488939b8c2fbe6d5e6f8da2ed6
Jackson Gomes <jgomes@ceulp.edu.br>
2020-05-15 10:48:24 -0300

    feat(frontend): define rotas
    Define rotas para login, perfil, sobre, rota padrão e página de erro.

--- lpweb20201-estagio-tcc-front/src/app/app-routing.module.ts
+++ lpweb20201-estagio-tcc-front/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PaginaNaoEncontradaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
